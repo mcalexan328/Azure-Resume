4e6ed53c48a8ff1c410236dbbecaed4fe25cab80
Mason Alexander <mcalexan01@gmail.com>
2024-08-15 10:23:34 -0500

    updated frontend

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionAPIUrl = 'https://azureresume813.azurewebsites.net/api/GetResumeCounter?code=rH8l6ihTsFdMZIpHXdpqFkcuv9yAWBp6ubx6FAaFxYdVAzFuq0ovDg%3D%3D';
+const functionAPIUrl = "https://azureresume813.azurewebsites.net/api/GetResumeCounter?code=rH8l6ihTsFdMZIpHXdpqFkcuv9yAWBp6ubx6FAaFxYdVAzFuq0ovDg%3D%3D";
 const localfunctionAPI= "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
